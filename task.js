2feeb9f2de457914e4801adf8217a35de3035ffd
premaxwells7 <premaxwells@gmail.com>
2025-06-21 16:28:35 +0100

    The task.js

--- task.js
+++ task.js
@@ -1,6 +1,6 @@
 function isEven(number)
 { return number % 2 === 0;}
-console.log(isEven(5)); //false
+console.log(isEven(5));//false
 console.log(isEven(10)); //true
 /* The isEven() function takes a (number) as input.
 it checks if number divided by 2 will give a remainder. (number % 2 == 0)
